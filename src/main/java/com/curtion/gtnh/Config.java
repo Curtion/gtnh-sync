50d895d4beb2db349b962eda753eb8a4de501b63
Curtion <curtion@126.com>
2023-01-19 14:27:31 +0800

    uuid to token

--- src/main/java/com/curtion/gtnh/Config.java
+++ src/main/java/com/curtion/gtnh/Config.java
@@ -7,21 +7,21 @@ import net.minecraftforge.common.config.Property;
 public class Config {
 
     private static class Defaults {
-        public static final String uuid = "curtion";
+        public static final String token = "default";
     }
 
     private static class Categories {
         public static final String general = "general";
     }
 
-    public static String uuid = Defaults.uuid;
+    public static String token = Defaults.token;
 
     public static void syncronizeConfiguration(File configFile) {
         Configuration configuration = new Configuration(configFile);
         configuration.load();
 
-        Property greetingProperty = configuration.get(Categories.general, "uuid", Defaults.uuid, "sync uuid");
-        uuid = greetingProperty.getString();
+        Property greetingProperty = configuration.get(Categories.general, "token", Defaults.token, "sync token");
+        token = greetingProperty.getString();
 
         if (configuration.hasChanged()) {
             configuration.save();
